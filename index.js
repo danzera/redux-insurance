5791e547b49c9420abf66d26c45938cbd61f111a
Dan <dczera@gmail.com>
2020-05-13 10:38:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-// ACTION CREATORS - simulate someone dropping off a form
+console.clear();
+
+// ACTION CREATORS - simulate someone dropping off an insurance form
 const createPolicy = (name, amount) => {
   // action to be returned
   return {
@@ -19,4 +21,42 @@ const createClaim = (name, dollarAmount) => {
     type: 'CREATE_CLAIM',
     payload: { name, dollarAmount }
   }
+}
+
+// REDUCERS - departments that take in the form
+// default oldListOfClaims in case it is undefined
+const claimsHistory(claimsList = [], action) => {
+  // we care about the action (form)
+  if (action.type === 'CREATE_CLAIM') {
+    // always return a new data structure as opposed to modifying existing data
+    // i.e. do NOT use oldListOfClaims.push(action.payload)
+    return [...claimsList, action.payload];
+  }
+  
+  // we don't care about the action (form)
+  return claimsList;
+}
+
+// arbitrarily setting default to 100 as an example
+const accounting = (totalDollars = 100, action) {
+  if (action.type === 'CREATE_CLAIM') {
+    return totalDollas - action.payload.dollarAmount;
+  } else if (action.type === 'CREATE_POLICY') {
+    return totalDollars + action.payload.amount;
+  }
+  
+  // default return
+  return totalDollars;
+}
+
+// existing policies
+const policies = (policyList = [], action) => {
+  if (action.type === 'CREATE_POLICY') {
+    return [...policyList, action.payload.name];
+  } else if (action.type === 'DELETE_POLICY') {
+    return policyList.filter(name => name !== action.payload.name);
+  }
+  
+  // default return
+  return policyList;
 }
